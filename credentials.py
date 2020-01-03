ec3b7fac83714c6bf8d4267a4965e6723a451df0
Amar-s-code <57569426+Amar-s-code@users.noreply.github.com>
2020-01-03 12:31:40 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-APITOKEN = "ySDbLRbiHH1gL74jVmlV4C0fRMJ4P2LC3kgGN1qJ" # Replace with Account API Token
-DATACENTER = "neu.co1"
-SPREADSHEET_ID = "16Ie4FK_xR-mGxEGq6Jex_sUcyVfvyb9Ai3UDpxRkGHI"
+APITOKEN = "" # Replace with Account API Token
+DATACENTER = ""
+SPREADSHEET_ID = ""
 DATASHEET="Hire_Salary.xlsx"
